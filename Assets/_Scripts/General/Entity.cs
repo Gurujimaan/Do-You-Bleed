97c90655b1ecfe902c39889a1146a8d11003450d
Gurujimaan <gabrugurmaan@gmail.com>
2026-05-28 17:32:13 +0530

    starter code

---
+++ Assets/_Scripts/General/Entity.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeamID
+{
+    Player,
+    Enemy
+}
+
+public abstract class Entity : MonoBehaviour
+{
+    public abstract TeamID TeamID { get; set; }
+    public abstract Entity Target { get; set; }
+    //public abstract List<AbilityDefinition> Abilities { get; }
+
+    public abstract float MaxHealth { get; set; }
+    public abstract float CurrentHealth { get; set; }
+    public abstract float Power { get; set; }
+    public abstract float Defense { get; set; }
+    public abstract float MovementSpeed { get; set; }
+
+    public abstract void TakeDamage(float amount);
+    public abstract void TakeHealing(float amount);
+    public abstract void Die();
+}
